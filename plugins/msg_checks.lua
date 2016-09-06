e71b743740ceb5d9d22227472f64c20b3e2ad613
nicesource <sourceeme@gmail.com>
2016-09-06 16:17:18 +0430

    Update msg_checks.lua

--- plugins/msg_checks.lua
+++ plugins/msg_checks.lua
@@ -1,11 +1,11 @@
---Begin msg_checks.lua
+-- fixed By aryan
 --Begin pre_process function
 local function pre_process(msg)
 -- Begin 'RondoMsgChecks' text checks by @rondoozle
 if is_chat_msg(msg) or is_super_group(msg) then
 	if msg and not is_momod(msg) and not is_whitelisted(msg.from.id) then --if regular user
 	local data = load_data(_config.moderation.data)
-	local print_name = user_print_name(msg.from):gsub("â€®", "") -- get rid of rtl in names
+	local print_name = user_print_name(msg.from):gsub("‮", "") -- get rid of rtl in names
 	local name_log = print_name:gsub("_", " ") -- name for log
 	local to_chat = msg.to.type == 'chat'
 	if data[tostring(msg.to.id)] and data[tostring(msg.to.id)]['settings'] then
@@ -32,6 +32,11 @@ if is_chat_msg(msg) or is_super_group(msg) then
 		lock_link = settings.lock_link
 	else
 		lock_link = 'no'
+	end
+	if settings.lock_member then
+		lock_member = settings.lock_member
+	else
+		lock_member = 'no'
 	end
 	if settings.lock_spam then
 		lock_spam = settings.lock_spam
@@ -77,7 +82,7 @@ if is_chat_msg(msg) or is_super_group(msg) then
 					kick_user(msg.from.id, msg.to.id)
 				end
 		end
-		if msg.service then
+		if msg.service then 
 			if lock_tgservice == "yes" then
 				delete_msg(msg.id, ok_cb, false)
 				if to_chat then
@@ -85,6 +90,7 @@ if is_chat_msg(msg) or is_super_group(msg) then
 				end
 			end
 		end
+		-- fixed By @omidi
 			local is_squig_msg = msg.text:match("[\216-\219][\128-\191]")
 			if is_squig_msg and lock_arabic == "yes" then
 				delete_msg(msg.id, ok_cb, false)
@@ -93,7 +99,7 @@ if is_chat_msg(msg) or is_super_group(msg) then
 				end
 			end
 			local print_name = msg.from.print_name
-			local is_rtl = print_name:match("â€®") or msg.text:match("â€®")
+			local is_rtl = print_name:match("‮") or msg.text:match("‮")
 			if is_rtl and lock_rtl == "yes" then
 				delete_msg(msg.id, ok_cb, false)
 				if strict == "yes" or to_chat then
@@ -101,6 +107,7 @@ if is_chat_msg(msg) or is_super_group(msg) then
 				end
 			end
 			if is_muted(msg.to.id, "Text: yes") and msg.text and not msg.media and not msg.service then
+			-- fixed By @omidi
 				delete_msg(msg.id, ok_cb, false)
 				if to_chat then
 					kick_user(msg.from.id, msg.to.id)
@@ -140,6 +147,7 @@ if is_chat_msg(msg) or is_super_group(msg) then
 					end
 				end
 			end
+			-- fixed By @omidi
 			if msg.media.caption then -- msg.media.caption checks
 				local is_link_caption = msg.media.caption:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/") or msg.media.caption:match("[Tt][Ll][Gg][Rr][Mm].[Mm][Ee]/")
 				if is_link_caption and lock_link == "yes" then
@@ -182,6 +190,7 @@ if is_chat_msg(msg) or is_super_group(msg) then
 					--	kick_user(msg.from.id, msg.to.id)
 				end
 			end
+			-- fixed By @omidi
 			local is_gif_caption =  msg.media.caption and msg.media.caption:match(".mp4")
 			if is_muted(msg.to.id, 'Gifs: yes') and is_gif_caption and msg.media.type:match("document") and not msg.service then
 				delete_msg(msg.id, ok_cb, false)
@@ -209,6 +218,7 @@ if is_chat_msg(msg) or is_super_group(msg) then
 				end
 			end
 		end
+		-- fixed By @omidi
 		if msg.fwd_from then
 			if msg.fwd_from.title then
 				local is_link_title = msg.fwd_from.title:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/") or msg.fwd_from.title:match("[Tt][Ll][Gg][Rr][Mm].[Mm][Ee]/")
@@ -244,11 +254,17 @@ if is_chat_msg(msg) or is_super_group(msg) then
 					end
 				end
 				local print_name = msg.from.print_name
-				local is_rtl_name = print_name:match("â€®")
+				local is_rtl_name = print_name:match("‮")
 				if is_rtl_name and lock_rtl == "yes" then
 					savelog(msg.to.id, name_log.." User ["..msg.from.id.."] joined and kicked (#RTL char in name)")
 					kick_user(user_id, msg.to.id)
 				end
+				if lock_member == 'yes' then
+					savelog(msg.to.id, name_log.." User ["..msg.from.id.."] joined and kicked (#lockmember)")
+					kick_user(user_id, msg.to.id)
+					delete_msg(msg.id, ok_cb, false)
+				end
+			end
 			if action == 'chat_add_user' and not is_momod2(msg.from.id, msg.to.id) then
 				local user_id = msg.action.user.id
 				if string.len(msg.action.user.print_name) > 70 and lock_group_spam == 'yes' then
@@ -261,11 +277,15 @@ if is_chat_msg(msg) or is_super_group(msg) then
 					end
 				end
 				local print_name = msg.action.user.print_name
-				local is_rtl_name = print_name:match("â€®")
+				local is_rtl_name = print_name:match("‮")
 				if is_rtl_name and lock_rtl == "yes" then
 					savelog(msg.to.id, name_log.." User ["..msg.from.id.."] added ["..user_id.."]: added user kicked (#RTL char in name)")
 					kick_user(user_id, msg.to.id)
 				end
+				if msg.to.type == 'channel' and lock_member == 'yes' then
+					savelog(msg.to.id, name_log.." User ["..msg.from.id.."] added ["..user_id.."]: added user kicked  (#lockmember)")
+					kick_user(user_id, msg.to.id)
+					delete_msg(msg.id, ok_cb, false)
 				end
 			end
 		end
@@ -279,6 +299,4 @@ return {
 	patterns = {},
 	pre_process = pre_process
 }
---End msg_checks.lua
---By @Rondoozle
-
+-- fixed By aryan
