5995f1247b1a6238d31a82296db8bde09ba4ed54
nicesource <sourceeme@gmail.com>
2016-09-07 23:09:13 +0430

    Update lock_fosh.lua

--- plugins/lock_fosh.lua
+++ plugins/lock_fosh.lua
@@ -1,25 +1,28 @@
-local function run(msg)
-   if msg.to.type == 'channel' and not is_momod(msg) then
-	delete_msg(msg.id,ok_cb,false)
-	else
-	kick_user(msg.from.id, msg.to.id)
-        return 'Do not swear'
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_fosh'] then
+                lock_fosh = data[tostring(msg.to.id)]['settings']['lock_fosh']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_fosh == "yes" then
+       delete_msg(msg.id, ok_cb, true)
     end
 end
-
+ 
 return {
-    patterns = {
-    "[Kk][Oo][Ss]",
-	"[Nn][Nn][Tt]",
-	"[Nn][Aa][Nn][Aa][Tt]",
-    "کص",
-    "کیر",
-	"کون",
-	"koon",
-	"ننت",
-	"ناموس",
-	"شل",
-    "کس ننه"
-    }, 
-run = run
+  patterns = {
+    
+  },
+  run = run
 }
+
+
+
