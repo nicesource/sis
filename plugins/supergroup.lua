e56988545d4616a75ed81d7af50ff36d4c3a60fc
nicesource <sourceeme@gmail.com>
2016-09-16 21:44:27 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1711,14 +1711,14 @@ end
 			end
 		end
 
-		if matches[1] == 'kick' or matches[1] == 'کیک' and is_momod(msg) then
+		if matches[1] == 'block' or matches[1] == 'اخراج' and is_momod(msg) then
 			if type(msg.reply_id) ~= "nil" then
 				local cbreply_extra = {
 					get_cmd = 'channel_block',
 					msg = msg
 				}
 				get_message(msg.reply_id, get_message_callback, cbreply_extra)
-			elseif matches[1] == 'kick' or matches[1] == 'کیک' and matches[2] and string.match(matches[2], '^%d+$') then
+			elseif matches[1] == 'block' or matches[1] == 'اخراج' and matches[2] and string.match(matches[2], '^%d+$') then
 				--[[local user_id = matches[2]
 				local channel_id = msg.to.id
 				if is_momod2(user_id, channel_id) and not is_admin2(user_id) then
@@ -1730,7 +1730,7 @@ end
 				local msg = msg
 				local user_id = matches[2]
 				channel_get_users (receiver, in_channel_cb, {get_cmd=get_cmd, receiver=receiver, msg=msg, user_id=user_id})
-			elseif matches[1] == "kick" or matches[1] == 'کیک' and matches[2] and not string.match(matches[2], '^%d+$') then
+			elseif matches[1] == "block" or matches[1] == "اخراج" and matches[2] and not string.match(matches[2], '^%d+$') then
 			--[[local cbres_extra = {
 					channelid = msg.to.id,
 					get_cmd = 'channel_block',
