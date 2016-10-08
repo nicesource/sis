8aae22e2b9fda01393626368a1972fb798e68bdf
nicesource <sourceeme@gmail.com>
2016-10-08 20:54:43 +0330

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -19,7 +19,7 @@ local function check_member_super(cb_extra, success, result)
         settings = {
           set_name = string.gsub(msg.to.title, '_', ' '),
 		  lock_arabic = 'no',
-		  lock_link = "no",
+		  lock_link = "yes",
           flood = 'yes',
 		  lock_spam = 'yes',
 		  lock_sticker = 'no',
