3c356c7845ba55f8825330914525bcada80ac4b7
nicesource <sourceeme@gmail.com>
2016-10-20 14:40:38 +0330

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,11 +1,11 @@
 do
     local function run(msg, matches)
-    local support = '1061279068' -- آیدی ساپورت بات رو اینجا قرار دهید
+    local support = '1051670668' 
     local data = load_data(_config.moderation.data)
     local name_log = user_print_name(msg.from)
         if matches[1] == 'support' or matches[1] == 'ساپورت' then
         local group_link = data[tostring(support)]['settings']['set_link']
-    return "Support Group link:\n"..group_link
+    return "<i>نرخ ربات ضدلینک و ضداسپم پاورشیلد به شرح زیر است •</i>\n\n<code>• یک ماهه : 3 هزار تومان</code>\n<code>•دوماهه : 6 هزار تومان</code>\n<code>•همیشگی : 10 هزار تومان</code>\n<i>++برای خرید مبلغ مورد نظر خود را به شماره کارت زیر بفرستید، سپس از فیش عکس گرفته وبه ساپورت ارسال کنید، بعد ادمین ها ربات رو در گروه شما اضافه میکنند</i>\n\n<code>شماره کارت:</code>\n6037701303422858\nبنام آرین قاسمی\n\n<code>لینک ساپورت: </code>\n"..group_link.."\n•توجه: شارژ قبول نمیشود"
     end
 end
 return {
